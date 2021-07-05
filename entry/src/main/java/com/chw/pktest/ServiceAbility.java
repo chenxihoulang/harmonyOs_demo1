06ca90b781fc30525108b9c1b1618cfbbc10ba77
chaihongwei <714652017@qq.com>
2021-07-05 17:01:21 +0800

    master add 添加Service生命周期回调方法

--- entry/src/main/java/com/chw/pktest/ServiceAbility.java
+++ entry/src/main/java/com/chw/pktest/ServiceAbility.java
@@ -1,7 +1,10 @@
 package com.chw.pktest;
 
 import ohos.aafwk.ability.Ability;
+import ohos.aafwk.ability.LocalRemoteObject;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.DependentLayout;
+import ohos.event.notification.NotificationRequest;
 import ohos.rpc.IRemoteObject;
 import ohos.hiviewdfx.HiLog;
 import ohos.hiviewdfx.HiLogLabel;
@@ -9,10 +12,34 @@ import ohos.hiviewdfx.HiLogLabel;
 public class ServiceAbility extends Ability {
     private static final HiLogLabel LABEL_LOG = new HiLogLabel(3, 0xD001100, "Demo");
 
+    /**
+     * 该方法在创建Service的时候调用，用于Service的初始化。在Service的整个生命周期只会调用一次，调用时传入的Intent应为空
+     */
     @Override
     public void onStart(Intent intent) {
-        HiLog.error(LABEL_LOG, "ServiceAbility::onStart");
+        HiLog.error(LABEL_LOG, "ServiceAbility::onStart,android onCreate");
         super.onStart(intent);
+
+        // 创建通知，其中1005为notificationId
+        NotificationRequest request = new NotificationRequest(1005);
+        NotificationRequest.NotificationNormalContent content = new NotificationRequest.NotificationNormalContent();
+        content.setTitle("title").setText("text");
+        NotificationRequest.NotificationContent notificationContent = new NotificationRequest.NotificationContent(content);
+        request.setContent(notificationContent);
+
+        // 绑定通知，1005为创建通知时传入的notificationId
+        keepBackgroundRunning(1005, request);
+    }
+
+    /**
+     * 在Service创建完成之后调用，该方法在客户端每次启动该Service时都会调用，用户可以在该方法中做一些调用统计、初始化类的操作
+     */
+    @Override
+    public void onCommand(Intent intent, boolean restart, int startId) {
+        HiLog.info(LABEL_LOG,"android onStartCommand");
+
+        //停止
+//        terminateAbility();
     }
 
     @Override
@@ -21,22 +48,39 @@ public class ServiceAbility extends Ability {
         HiLog.info(LABEL_LOG, "ServiceAbility::onBackground");
     }
 
+    /**
+     * 在Service销毁时调用。Service应通过实现此方法来清理任何资源，如关闭线程、注册的侦听器等
+     */
     @Override
     public void onStop() {
         super.onStop();
-        HiLog.info(LABEL_LOG, "ServiceAbility::onStop");
+        HiLog.info(LABEL_LOG, "ServiceAbility::onStop,android onDestroy");
     }
 
-    @Override
-    public void onCommand(Intent intent, boolean restart, int startId) {
-    }
-
+    /**
+     * 在Ability和Service连接时调用，该方法返回IRemoteObject对象，
+     * 用户可以在该回调函数中生成对应Service的IPC通信通道，以便Ability与Service交互。
+     * Ability可以多次连接同一个Service，系统会缓存该Service的IPC通信对象，
+     * 只有第一个客户端连接Service时，系统才会调用Service的onConnect方法来生成IRemoteObject对象，
+     * 而后系统会将同一个RemoteObject对象传递至其他连接同一个Service的所有客户端，而无需再次调用onConnect方法
+     *
+     * connectAbility()
+     */
     @Override
     public IRemoteObject onConnect(Intent intent) {
-        return null;
+        HiLog.info(LABEL_LOG,"android onBind");
+        return new MyRemoteObject();
     }
 
+    /**
+     * 在Ability与绑定的Service断开连接时调用,disconnectAbility()
+     */
     @Override
     public void onDisconnect(Intent intent) {
+        HiLog.info(LABEL_LOG,"android onUnbind");
+    }
+
+    private class MyRemoteObject extends LocalRemoteObject{
+
     }
 }
--- entry/src/main/java/com/chw/pktest/slice/HelloWorldAbilitySlice.java
+++ entry/src/main/java/com/chw/pktest/slice/HelloWorldAbilitySlice.java
@@ -3,19 +3,24 @@ package com.chw.pktest.slice;
 import com.chw.pktest.HelloWorldAbility;
 import com.chw.pktest.MainAbility;
 import com.chw.pktest.ResourceTable;
+import com.chw.pktest.ServiceAbility;
 import com.chw.pktest.utils.LogUtils;
 import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.ability.IAbilityConnection;
 import ohos.aafwk.ability.IAbilityContinuation;
 import ohos.aafwk.ability.startsetting.AbilityStartSetting;
 import ohos.aafwk.content.Intent;
 import ohos.aafwk.content.IntentParams;
+import ohos.aafwk.content.Operation;
 import ohos.agp.components.Button;
 import ohos.agp.components.Component;
 import ohos.bundle.ElementName;
+import ohos.rpc.IRemoteObject;
 import ohos.security.NetworkSecurityPolicy;
 
 public class HelloWorldAbilitySlice extends AbilitySlice implements IAbilityContinuation {
     private static final String TAG = HelloWorldAbilitySlice.class.getSimpleName();
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
@@ -29,15 +34,47 @@ public class HelloWorldAbilitySlice extends AbilitySlice implements IAbilityCont
 
         Button btnNavAbility = (Button) findComponentById(ResourceTable.Id_btnNavAbility);
         btnNavAbility.setClickedListener(listener -> {
-            Intent intent1=new Intent();
+            Intent intent1 = new Intent();
             ElementName elementName = new ElementName();
             elementName.setAbilityName(MainAbility.class.getName());
             intent1.setElement(elementName);
 
 //            startAbility(intent1);
-            startAbilityForResult(intent1,1, AbilityStartSetting.getEmptySetting());
+            startAbilityForResult(intent1, 1, AbilityStartSetting.getEmptySetting());
+        });
+
+        Button btnStartService = (Button) findComponentById(ResourceTable.Id_btnStartService);
+        btnStartService.setClickedListener(listener -> {
+            Intent intent1 = new Intent();
+            Operation operation = new Intent.OperationBuilder()
+                    .withDeviceId("")
+                    .withBundleName(ServiceAbility.class.getPackage().getName())
+                    .withAbilityName(ServiceAbility.class)
+                    .build();
+            intent1.setOperation(operation);
+
+//            startAbility(intent1);
+
+            // 连接Service
+            connectAbility(intent1,connection);
+            //断开连接
+            disconnectAbility(connection);
         });
     }
+
+    // 创建连接Service回调实例
+    private IAbilityConnection connection = new IAbilityConnection() {
+        // 连接到Service的回调
+        @Override
+        public void onAbilityConnectDone(ElementName elementName, IRemoteObject iRemoteObject, int resultCode) {
+            // Client侧需要定义与Service侧相同的IRemoteObject实现类。开发者获取服务端传过来IRemoteObject对象，并从中解析出服务端传过来的信息。
+        }
+
+        // Service异常死亡的回调
+        @Override
+        public void onAbilityDisconnectDone(ElementName elementName, int resultCode) {
+        }
+    };
 
     /**
      * presentForResult请求的返回结果
@@ -46,7 +83,7 @@ public class HelloWorldAbilitySlice extends AbilitySlice implements IAbilityCont
     protected void onResult(int requestCode, Intent resultIntent) {
         super.onResult(requestCode, resultIntent);
         if (requestCode == 1) {
-            LogUtils.info(TAG,"android onActivityResult");
+            LogUtils.info(TAG, "android onActivityResult");
         }
     }
 
@@ -72,7 +109,7 @@ public class HelloWorldAbilitySlice extends AbilitySlice implements IAbilityCont
     protected void onBackground() {
         super.onBackground();
     }
-    
+
     @Override
     public boolean onStartContinuation() {
         return false;
